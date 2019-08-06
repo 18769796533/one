3b94cc9567b7b39cad0b285e0cbc3bed098bb6a9
18769796533 <532941598@qq.com>
2019-08-07 00:11:36 +0800

    第二天第二次上传，修改了ajax请求数据

---
+++ zhg/js/index.js
@@ -0,0 +1,51 @@
+$(".banner1").banner({
+    items:$(".imgbox").find("div"),        //必传
+    left:$(".imgbtns").find("#left"),       //可选
+    right:$(".imgbtns").find("#right"),     //可选
+    list:true,
+    autoPlay:true,                          //可选，默认有自动播放
+    delayTime:3000,                         //可选，默认2000
+    moveTime:600,                          //可选，默认300
+    index:0,                                //可选，默认0
+})
+
+
+
+
+
+
+
+class Sylist{
+    constructor(){
+        this.sylist = document.querySelector(".sylist");
+        this.url = "http://localhost/zhg/data/goods.json";
+        this.load()
+    }
+    load(){
+        var that = this;
+        ajaxGet(this.url,function(res){
+            that.res = JSON.parse(res);
+            that.display();
+        })
+    }
+    display(){
+        var str = "";
+        for(var i = 0;i<this.res.length;i++){
+            str += `<li>
+                        <div>
+                            <img src="${this.res[i].url}" alt="">
+                            <h4>${this.res[i].name}</h4>
+                            <p>${this.res[i].tip}</p>
+                            <em>${this.res[i].price}</em>
+                        </div>
+                    </li>`
+        }
+        this.sylist.innerHTML = str;
+    }
+}
+new Sylist();
+
+
+
+
+
---
+++ zhg/js/load.js
@@ -0,0 +1,52 @@
+$(".topNav").load("http://localhost/zhg/load.html .one",);
+$(".logoNav").load("http://localhost/zhg/load.html .two",);
+$(".nav").load("http://localhost/zhg/load.html .three",fn);
+$(".bvip").load("http://localhost/zhg/load.html .four",);
+$(".hot").load("http://localhost/zhg/load.html .five",);
+$(".fotter_b").load("http://localhost/zhg/load.html .six",);
+$(".end").load("http://localhost/zhg/load.html .seven",);
+
+function fn(){ 
+    //导航条鼠标滑过
+    $(".navbox2").find("li").hover(function(){
+        $(this).css({
+            background:"#00BBD7"
+        })
+    },function(){
+        $(this).css({
+            background:"#33353d"
+        })
+        
+    })
+    $(".navbox").hover(function(){
+        $(this).find("ul").show()
+    },function(){
+        $(this).find("ul").hide()
+    })
+    //导航列表划过效果
+    $(".navbox ul li").hover(function(){
+        $(this).css({
+            background:"#ffffff",
+            color:"#00BBD7"
+        }).find("i").css({
+            color:"#00BBD7"
+        }).parent().siblings(".nav_b").show()
+    },function(){
+        $(this).css({
+            background:"#D7D7D9",
+            color:"#333333"
+        }).find("i").css({
+            color:"#333333"
+        }).parent().siblings(".nav_b").hide()
+    })
+
+    $(".nav_b3").find("p").hover(function(){
+        $(this).find("a").css({
+            color:"#00BBD7"
+        })
+    },function(){
+        $(this).find("a").css({
+            color:"#666"
+        })
+    })
+}
